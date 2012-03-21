36f9f34c07f7b9bc7ceb062a165a0d44fe7abbc5
walidvb <walidvb@gmail.com>
2012-03-21 18:37:15 +0100

    check in, camera working and saving

--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -1,13 +1,36 @@
 package camera;
 
+import controlP5.ControlEvent;
 import processing.core.PApplet;
+import processing.core.PImage;
+import processing.video.Capture;
 
 
 public class Camera extends PApplet {
-
+	Capture myCam;
+	String path = selectFolder("Select your destination folder:") +"/";
+	int picNumber = 1;
 	public void setup() {
+		  myCam = new Capture(this, width, height, 30);
+
 	}
 
-	public void draw() {
+	public void draw() 
+	{
+		myCam.read();
+		image(myCam, 0, 0);
+	}
+	
+	public void keyPressed()
+	{
+		if(key == 'n')
+		{
+			myCam.read();
+			String imgName = "face";
+			String destFile = path + imgName +"_"+ picNumber++ + ".jpeg";
+			PImage myFace = myCam;
+			println(destFile);
+			save(destFile);
+		}
 	}
 }
