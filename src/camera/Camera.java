8f38e6fc929680c11f7e1b3ff29f0300eb5c6852
walidvb <walidvb@gmail.com>
2012-03-22 15:38:57 +0100

    added flash

--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -8,33 +8,60 @@ import processing.video.Capture;
 public class Camera extends PApplet {
 	Capture myCam;
 	String path = selectFolder("Select your destination folder:") +"/";
+	PImage face;
 	int picNumber = 1;
+	int flash = 0;
 	public void setup() {
-		myCam = new Capture(this, 500, 500, 30);
+		size(380, 640);
+
+		myCam = new Capture(this, 380, 640, 30);
+		face = loadImage("face2.png");
+		//face.resize(this.width, this.height);
+		println(myCam.width + " " + myCam.height);
 	}
 
 	public void draw() 
 	{
-		size(500, 500);
 		myCam.read();
-		image(myCam, 0, 0);
-		ellipse(width/2, height/2, 50, 50);
+		/*
+		 * PImage myCamRight = myCam;
+		myCamRight.loadPixels();
+		myCam.loadPixels();
 
+		for(int i = 1; i < myCam.pixels.length; i++)
+		{
+			myCamRight.pixels[i-1] = myCam.pixels[myCam.pixels.length-i];
+		}
+		myCamRight.updatePixels();
+		*/
+		image(myCam, 0, 0);
+
+		image(face, -150, 0);
+		
+		if(flash > 0)
+		{
+			rect(0, 0, this.width, this.height);
+			fill(255, flash);
+			flash-=10;
+		}
 	}
 
 	public void keyPressed()
 	{
-		if(key == 'a')
+		if(key == 'a' || key == ' ')
 		{
-			if(myCam.available())
+			
+			if(!myCam.available())
 			{
 				myCam.read();
+
 				String imgName = "face";
 				String destFile = path + imgName +"_"+ picNumber + ".jpeg";
 				picNumber++;
 				println(destFile);
-				save(destFile);
+				myCam.save(destFile);
 			}
+			flash = 255;
 		}
 	}
 	
